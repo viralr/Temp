3d5aa4e4b1a75405c65eef6d53b04f016a99dfc2
viralr <viralr@VIRALR-DPM4800>
2016-09-01 09:20:55 -0700

    c

---
+++ Webhooktest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webhooktest.Startup))]
+namespace Webhooktest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
